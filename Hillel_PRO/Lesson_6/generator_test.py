fe2e5a171301256413d91f319607de22277ecf84
Volodymyr Saratovskyy <sva.in.ua@gmail.com>
2024-01-31 13:22:31 +0100

    homework 6 is done

---
+++ Hillel_PRO/Lesson_6/generator_test.py
@@ -0,0 +1,33 @@
+# A Python program to generate squares from 1
+# to 100 using yield and therefore generator
+
+# An infinite generator function that prints
+# next square number. It starts with 1
+
+
+def nextSquare():
+    i = 1
+
+    # An Infinite loop to generate squares
+    while True:
+        yield i * i
+        i += 1  # Next execution resumes
+        # from this point
+
+
+# Driver code to test above generator
+# function
+for num in nextSquare():
+    if num > 100:
+        break
+    print(num)
+
+
+# def square_generator(n):
+#     for i in range(n):
+#         yield i ** 2
+
+# squares = square_generator(5)
+
+# for square in squares:
+#     print(square)
