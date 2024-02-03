f890927bea9829c1b94b60b6155f92a69b44a2d1
Volodymyr Saratovskyy <sva.in.ua@gmail.com>
2024-02-03 08:27:03 +0100

    HW done

--- Hillel_PRO/Lesson_7/HW/lesson_7_task_1.py
+++ Hillel_PRO/Lesson_7/HW/lesson_7_task_1.py
@@ -10,14 +10,19 @@ team: list[dict] = [
 def repr_players(players):
     for player in players:
         print(
-            f"[Player {player['number']}]: {player['name']}, {player['age']}"
+            f"\t[Player {player['number']}]: {player['name']}, {player['age']}"
         )
 
 
 def player_add(name: str, age: int, number: int) -> dict:
     player: dict = {"name": name, "age": age, "number": number}
-    team.append(player)
-    return player
+    for person in team:
+        if person["number"] == number:
+            print("This number is occupied, choose another one")
+            return player
+    else:
+        team.append(player)
+        return player
 
 
 def player_delete(number: int) -> None:
@@ -26,28 +31,50 @@ def player_delete(number: int) -> None:
             del player
 
 
+def player_update(name: str, age: int, number: int) -> dict:
+    for person in team:
+        if person["number"] == number:
+            person["name"] = name
+            person["age"] = age
+
+
 def main():
-    operations = ("add", "del", "repr")
+    operations = ("add", "upd", "repr", "del", "exit")
     while True:
         operation = input("Please enter the operation: ")
         if operation not in operations:
             print(f"Operation: {operation} is not available\n")
             continue
 
-        if operation == "repr":
+        if operation == "exit":
+            print("Bye")
+            break
+        elif operation == "repr":
             repr_players(team)
         elif operation == "add":
             user_data = input(
                 "Enter new player information [name, age, number]: "
             ).split(",")
             name, age, number = user_data
+
             try:
                 player_add(name, int(age), int(number))
             except ValueError:
                 print("Age and player's number must be integers\n\n")
                 continue
-        # elif operation == "del":
-        #     player_delete()
+        elif operation == "upd":
+            number_to_update = input(
+                "Enter the player's number whose data you want to update: "
+            )
+            name_and_age_to_update = input(
+                "Enter new player information [name, age] :"
+            ).split(",")
+            new_name, new_age = name_and_age_to_update
+            try:
+                player_update(new_name, int(new_age), int(number_to_update))
+            except ValueError:
+                print("Age and player's number must be integers\n\n")
+                continue
         else:
             raise NotImplementedError
 
--- Hillel_PRO/Lesson_7/HW/test.py
+++ Hillel_PRO/Lesson_7/HW/test.py
@@ -1,16 +1,22 @@
-# words = "Klark,22,13".split(",")
-
-# *data, number = words
-
 # print(number)
 
 
-team: list[dict] = [
-    {"name": "John", "age": 20, "number": 1},
-    {"name": "Mark", "age": 33, "number": 3},
-    {"name": "Kavin", "age": 31, "number": 12},
-]
+# team: list[dict] = [
+#     {"name": "John", "age": 20, "number": 1},
+#     {"name": "Mark", "age": 33, "number": 3},
+#     {"name": "Kavin", "age": 31, "number": 12},
+# ]
 
 
-for player in team:
-    print(f"[Player {player['number']}]: {player['name']}, {player['age']}")
+# for player in team:
+#     print(f"[Player {player['number']}]: {player['name']}, {player['age']}")
+
+
+# words = "Klark,22,12".split(",")
+# *data, number = words
+# print(number)
+
+
+# for check in team:
+#     if check["number"] == int(number):
+#         print("occupied")
