cb593c5e5e464b0ab6afd8a2f7657a5c8596cb57
Volodymyr Saratovskyy <sva.in.ua@gmail.com>
2024-02-14 18:08:23 +0100

    HW is done

--- Hillel_PRO/Lesson_10/currency_prep_api.py
+++ Hillel_PRO/Lesson_10/currency_prep_api.py
@@ -1,9 +1,16 @@
+import datetime
+import json
 from dataclasses import dataclass
 
 import requests
 
 MIDDLE_CURRENCY = "CHF"
-ALPHA_VANTAGE_API_KEY = "XSN17SDSA5RAM5W2"
+ALPHA_VANTAGE_API_KEY = "H7CYK4OLE6Z4MEN0"
+
+fulldate = datetime.datetime.now()
+timestamp: str = fulldate.strftime("%d/%m/%Y at %H:%M:%S")
+
+results = {"results": []}
 
 
 @dataclass
@@ -39,14 +46,30 @@ class Price:
         return Price(value=total_in_left_currency, currency=self.currency)
 
 
+def track_result(currency_from: str, currency_to: str, rate: float, date: str):
+    content: dict = {
+        "currency_from": currency_from,
+        "currency_to": currency_to,
+        "rate": rate,
+        "timestamp": date,
+    }
+    results["results"].append(content)
+
+
 def convert(value: float, currency_from: str, currency_to: str) -> float:
-    URL = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={currency_from}&to_currency={currency_to}&apikey={ALPHA_VANTAGE_API_KEY}"
+    URL = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={currency_from}&to_currency={currency_to}&apikey={ALPHA_VANTAGE_API_KEY}"  # noqa
     response: requests.Response = requests.get(URL)
     result: dict = response.json()
     coefficient: float = float(
         result["Realtime Currency Exchange Rate"]["5. Exchange Rate"]
     )
-    return value * coefficient
+    track_result(
+        currency_from=currency_from,
+        currency_to=currency_to,
+        rate=coefficient,
+        date=timestamp,
+    )
+    return round(value * coefficient, 2)
 
 
 flight = Price(value=200, currency="USD")
@@ -54,3 +77,6 @@ hotel = Price(value=1000, currency="UAH")
 
 total: Price = flight + hotel
 print(total)
+
+with open("logs.json", mode="a") as file:
+    json.dump(results, file, indent=4)
---
+++ Hillel_PRO/Lesson_10/test.py
@@ -0,0 +1,35 @@
+import datetime
+import json
+
+results = {"results": []}
+
+def write_to_file(currency_from: str, currency_to: str, rate: str, date: str): #noqa
+    content: dict = {
+        "currency_from": currency_from,
+        "currency_to": currency_to,
+        "rate": rate,
+        "timestamp": date,
+    }
+    # with open("logs.json", mode="a") as file:
+    #     json.dump(content, file)
+    results["results"].append(content)
+
+
+currency_from: str = "UAH"
+currency_to: str = "USD"
+rate: str = "0.025"
+
+fulldate = datetime.datetime.now()
+date_and_time: str = fulldate.strftime("%d/%m/%Y at %H:%M:%S")
+
+write_to_file(
+    currency_from=currency_from,
+    currency_to=currency_to,
+    rate=rate,
+    date=date_and_time,
+)
+
+with open("logs.json", mode="a") as file:
+    json.dump(results, file, indent=4)
+
+
