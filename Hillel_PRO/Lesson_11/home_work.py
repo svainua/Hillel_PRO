208e32a7f642ad92de60cd95bd65bd3c905b49b3
Volodymyr Saratovskyy <sva.in.ua@gmail.com>
2024-02-22 22:33:24 +0100

    current time is added to the ABC class

--- Hillel_PRO/Lesson_11/home_work.py
+++ Hillel_PRO/Lesson_11/home_work.py
@@ -13,6 +13,8 @@ class SocialChannel(ABC):
     def __init__(self, network: str, follower: int) -> None:
         self.network = network
         self.follower = follower
+        self.current_time = datetime.now()
+        self.current_hour = int(self.current_time.strftime("%H"))
 
     @abstractmethod
     def make_a_post(self, message):
@@ -38,25 +40,22 @@ def post_a_message(channel: SocialChannel, message: str) -> None:
     channel.make_a_post(message)
 
 
-def time() -> int:
-    current_time = datetime.now()
-    return int(current_time.strftime("%H"))
+# def time() -> int:
+#     current_time = datetime.now()
+#     return int(current_time.strftime("%H"))
 
 
 def process_schedule(posts: list[Post], channels: list[SocialChannel]) -> None:
     for post in posts:
         message, timestamp = post.message, post.timestamp
         for channel in channels:
-            if timestamp <= time():
+            if timestamp <= channel.current_hour:
                 post_a_message(channel, message)
 
 
-current_time = datetime.now()
-hour = current_time.strftime("%H")
-
 post_1 = Post(message="Hello today", timestamp=10)
-post_2 = Post(message="Hello tomorrow", timestamp=13)
-post_3 = Post(message="Hello day after tomorrow", timestamp=17)
+post_2 = Post(message="Hello tomorrow", timestamp=24)
+post_3 = Post(message="Hello day after tomorrow", timestamp=24)
 
 process_schedule(
     posts=[post_1, post_2, post_3],
