d9c9c8edf6141493f8584e8d810e0e408d6ad046
Volodymyr Saratovskyy <sva.in.ua@gmail.com>
2024-02-26 08:59:24 +0100

    HW is updated

--- Hillel_PRO/Lesson_12/homework.py
+++ Hillel_PRO/Lesson_12/homework.py
@@ -2,7 +2,7 @@ import os
 import threading
 import time
 from dataclasses import dataclass
-from multiprocessing import Process
+from multiprocessing import Process, Queue
 
 import requests
 from cryptography.fernet import Fernet
@@ -13,7 +13,8 @@ class Path:
     path: str
 
 
-def encrypt_file(path: str):
+def encrypt_file(path: str, queue: Queue):
+    start_proc = time.perf_counter()
     print(f"Processing file from {path} in process {os.getpid()}")
     key = Fernet.generate_key()
     with open("filekey.key", "wb") as filekey:
@@ -26,9 +27,13 @@ def encrypt_file(path: str):
     encrypted = fernet.encrypt(original)
     with open(path, "wb") as encrypted_file:
         encrypted_file.write(encrypted)
+    global encryption_counter
+    encryption_counter = time.perf_counter() - start_proc
+    queue.put(encryption_counter)
 
 
-def download_image(image_url, image_path):
+def download_image(image_url, image_path, queue):
+    start_thread = time.perf_counter()
     print(
         f"Downloading image from {image_url} "
         f"in thread {threading.current_thread().name}"
@@ -36,6 +41,9 @@ def download_image(image_url, image_path):
     response = requests.get(image_url)
     with open(image_path, "wb") as f:
         f.write(response.content)
+    global download_counter
+    download_counter = time.perf_counter() - start_thread
+    queue.put(download_counter)
 
 
 text_file_path = Path(path="rockyou.txt")
@@ -46,19 +54,25 @@ url_image_path = Path(path="https://picsum.photos/1000/1000")
 if __name__ == "__main__":
 
     try:
-        start_proc = time.perf_counter()
-        my_proc = Process(target=encrypt_file, args=(text_file_path.path,))
-        my_proc.start()
-        my_proc.join()
-        encryption_counter = time.perf_counter() - start_proc
+        encryption_queue = Queue()
+        download_queue = Queue()
 
-        start_thread = time.perf_counter()
-        my_thread = threading.Thread(
-            target=download_image, args=(url_image_path.path, image_path.path)
+        my_proc = Process(
+            target=encrypt_file, args=(text_file_path.path, encryption_queue)
         )
+        my_thread = threading.Thread(
+            target=download_image,
+            args=(url_image_path.path, image_path.path, download_queue),
+        )
+
+        my_proc.start()
         my_thread.start()
+
+        my_proc.join()
         my_thread.join()
-        download_counter = time.perf_counter() - start_thread
+
+        encryption_counter = encryption_queue.get()
+        download_counter = download_queue.get()
 
         total = encryption_counter + download_counter
 
